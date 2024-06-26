12064abbdd5d3c429a70ddf055d4e1493e2ba055
KazuakiShigefuji <shigefuji@gmail.com>
2024-06-26 10:43:41 +0900

    Bugfix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('changeImageButton').addEventListener('click', function() {
     var image = document.getElementById('interactiveImage');
-    if (image.src.includes('Image1.jpg')) {
+    if (image.src.includes('Image1.jfif')) {
         image.src = 'Image2.jfif';
     } else {
         image.src = 'Image1.jfif';
